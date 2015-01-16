ee6c700f99e20d63c7384e744fdc4a2a7992c6cd
Robert Pocklington <rpocklin@gmail.com>
2015-01-16 22:53:14 +1100

    Removed unused code from example controller.

--- example/example-controller.js
+++ example/example-controller.js
@@ -1,17 +1,6 @@
 'use strict';
 
 var ExampleCtrl = function($rootScope, $scope) {
-
-  $scope.initialise = function() {
-    $scope.order = [
-      { state: 'created', description: 'Created online via Ebay', on: '1/1/15' },
-      { state: 'packaged', description: 'Created online via Ebay', on: '2/1/15' },
-      { state: 'sent', description: 'Created online via Ebay', on: '4/1/15' },
-      { state: 'delivered', description: 'Confirmed delivery via EMS', on: '6/1/15' }
-    ]
-  };
-
-  $scope.initialise();
 };
 
 angular.module('example').controller('ExampleCtrl', ExampleCtrl);
