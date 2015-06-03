51578779f86ae2f03a5bb48d5bd35b5b0209a2cb
Robert Pocklington <rpocklin@gmail.com>
2015-06-04 08:05:40 +1000

    Added ngdocs.

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,70 @@
+NG_DOCS={
+  "sections": {
+    "api": "API"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "angular-timeline",
+      "shortName": "angular-timeline",
+      "type": "directive",
+      "moduleName": "angular-timeline",
+      "shortDescription": "Primary container for displaying a vertical set of timeline events.",
+      "keywords": "ae angular-timeline api container directive displaying events primary set timeline vertical"
+    },
+    {
+      "section": "api",
+      "id": "angular-timeline.directive:timeline",
+      "shortName": "timeline",
+      "type": "directive",
+      "moduleName": "angular-timeline",
+      "shortDescription": "Represents an event occuring at a point in time, displayed on the left or the right",
+      "keywords": "ae angular-timeline api directive displayed element embed event left occuring point represents time timeline timeline-badge timeline-event timeline-panel typically"
+    },
+    {
+      "section": "api",
+      "id": "angular-timeline.directive:timeline-badge",
+      "shortName": "timeline-badge",
+      "type": "directive",
+      "moduleName": "angular-timeline",
+      "shortDescription": "Shown in the centre pane (or left on narrow devices) to indicate the activity.",
+      "keywords": "activity ae angular-timeline api centre devices directive indicate left narrow pane"
+    },
+    {
+      "section": "api",
+      "id": "angular-timeline.directive:timeline-footer",
+      "shortName": "timeline-footer",
+      "type": "directive",
+      "moduleName": "angular-timeline",
+      "shortDescription": "Optional element to add a footer section to the timeline-panel for links or other actions.",
+      "keywords": "actions add ae angular-timeline api directive element footer links optional timeline-panel"
+    },
+    {
+      "section": "api",
+      "id": "angular-timeline.directive:timeline-heading",
+      "shortName": "timeline-heading",
+      "type": "directive",
+      "moduleName": "angular-timeline",
+      "shortDescription": "Optional element to show the heading for a timeline-panel.",
+      "keywords": "ae angular-timeline api directive element heading optional timeline-panel"
+    },
+    {
+      "section": "api",
+      "id": "angular-timeline.directive:timeline-panel",
+      "shortName": "timeline-panel",
+      "type": "directive",
+      "moduleName": "angular-timeline",
+      "shortDescription": "An panel inside the timeline-event which shows detailed information about the event.",
+      "keywords": "ae angular-timeline api detailed directive event inside panel timeline-event"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api/timeline",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
