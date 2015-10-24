6a82e1ba9a06456a6554e70931590357b8e6f136
Laurens Van Keer <laurens@snoozon.com>
2015-10-24 18:45:07 +0200

    Added possibility to define custom position for event elements

--- src/timeline-event-directive.js
+++ src/timeline-event-directive.js
@@ -10,12 +10,28 @@
  * of the timeline line.
  *
  * You typically embed a `timeline-badge` and `timeline-panel` element within a `timeline-event`.
+ *
+ * @param {string=} side  Define the side of the element (i.e. side="left", side="right", or use an expression).
  */
 angular.module('angular-timeline').directive('timelineEvent', function() {
   return {
     require: '^timeline',
     restrict: 'AE',
     transclude: true,
-    template: '<li ng-class-even="\'timeline-inverted\'" ng-transclude></li>'
+    template: '<li ng-transclude></li>',
+    link: function(scope, element, attrs) {
+
+      var liElt = element.find('li');
+      if ('undefined' !== typeof attrs.side) {
+        switch (attrs.side) {
+          case 'right':
+            liElt.addClass('timeline-inverted');
+            break;
+          default:
+            liElt.removeClass('timeline-inverted');
+        }
+      }
+
+    }
   };
 });
